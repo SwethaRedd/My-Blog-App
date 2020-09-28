cb13f4b5528363a95db780f6b68459e716ac26c6
SwethaRedd <swethareddy.nadipi@gmail.com>
2020-09-28 17:27:38 -0400

    Final Commit
    Final Commit

--- js/index.js
+++ js/index.js
@@ -2,44 +2,41 @@ const API_URL = "http://localhost:3000/api/posts";
 const API_BASE_URL = "http://localhost:3000/";
 
 window.onload = () => {
-  getPosts();
-};
+    getPosts();
+}
 
 const getPosts = () => {
-  fetch(API_URL, {
-    method: "GET",
-  })
-    .then((response) => {
-      return response.json();
+    fetch(API_URL, {
+        method: 'GET'
+    }).then((response)=>{
+        return response.json();
+    }).then((data)=>{
+        buildPosts(data);
     })
-    .then((data) => {
-      buildPosts(data);
-    });
-};
+}
 
 const buildPosts = (blogPosts) => {
-  console.log(blogPosts);
-  let blogPostContent = "";
-  for (blogPost of blogPosts) {
-    const postDate = new Date(parseInt(blogPost.added_date)).toDateString();
-    const postImage = `${API_BASE_URL}${blogPost.post_image}`;
-    const postLink = `/post.html?id=${blogPost.id}`;
-    blogPostContent += `
-    <a class="post-link" href="${postLink}">
-        <div class="post">
-            <div class="post-image" style="background-image: url(${postImage})"></div>
-            <div class="post-content">
-                <div class="post-date">${postDate}</div>
-                <div class="post-title">
-                    <h4>${blogPost.title}</h4>
+    let blogPostsContent = "";
+    for(blogPost of blogPosts){
+        const postImage = API_BASE_URL + blogPost.post_image;
+        const postLink = `/post.html?id=${blogPost.id}`;
+        const postDate = new Date(parseInt(blogPost.added_date)).toDateString();
+        blogPostsContent += `
+            <a class="post-link" href="${postLink}">
+                <div class="post">
+                    <div class="post-image" style="background-image: url(${postImage})"></div>
+                    <div class="post-content">
+                        <div class="post-date">${postDate}</div>
+                        <div class="post-title">
+                            <h4>${blogPost.title}</h4>
+                        </div>
+                        <div class="post-text">
+                            <p>${blogPost.content}</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="post-text">
-                    <p>${blogPost.content}</p>
-                </div>
-            </div>
-        </div>
-    </a>
-`;
-  }
-  document.querySelector(".blog-posts").innerHTML = blogPostContent;
-};
+            </a>
+        `
+    }
+    document.querySelector(".blog-posts").innerHTML = blogPostsContent;
+}
